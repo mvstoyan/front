7d04381e5d44bc21bd273290720ac1c13ebd659c
Marina Tilton <m.v.stoyan@gmail.com>
2023-06-14 20:21:56 -0700

    login

--- src/App.js
+++ src/App.js
@@ -1,151 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Auth from './components/Auth';
 
-const rootUrl = 'https://e-commerce-silkland.onrender.com/api/v1';
-
-function App() {
-  const [registerData, setRegisterData] = useState({
-    firstName: '',
-    email: '',
-    password: '',
-  });
-
-  const [loginData, setLoginData] = useState({
-    email: '',
-    password: '',
-  });
-
-  const handleRegister = async (e) => {
-    e.preventDefault();
-
-    try {
-      const url = `${rootUrl}/auth/register`;
-      const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(registerData),
-      });
-
-      if (response.ok) {
-        console.log('Registration successful');
-      } else {
-        console.log('Error during registration');
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const handleLogin = async (e) => {
-    e.preventDefault();
-
-    try {
-      const url = `${rootUrl}/auth/login`;
-      const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(loginData),
-      });
-
-      if (response.ok) {
-        console.log('Login successful');
-      } else {
-        console.log('Error during login');
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
+const App = () => {
   return (
-    <>
-      <form className="form" onSubmit={handleRegister}>
-        <h4>Registration Form</h4>
-        <div className="form-row">
-          <label htmlFor="firstName" className="form-label">
-            Name
-          </label>
-          <input
-            type="text"
-            id="firstName"
-            className="form-input"
-            value={registerData.firstName}
-            onChange={(e) =>
-              setRegisterData({ ...registerData, firstName: e.target.value })
-            }
-          />
-        </div>
-        <div className="form-row">
-          <label htmlFor="email" className="form-label">
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            className="form-input"
-            value={registerData.email}
-            onChange={( e) => setRegisterData({ ...registerData, email: e.target.value })
-            }
-          />
-        </div>
-        <div className="form-row">
-          <label htmlFor="password" className="form-label">
-          Password
-          </label>
-          <input
-            type="password"
-            id="password"
-            className="form-input"
-            value={registerData.password}
-            onChange={(e) =>
-              setRegisterData({ ...registerData, password: e.target.value })
-            }
-          />
-        </div>
-        <button type="submit" className="btn btn-block submit-btn">
-        Register
-        </button>
-      </form>
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/auth">Auth</Link>
+      </nav>
 
-      <form className="form" onSubmit={handleLogin}>
-        <h4>Login Form</h4>
-        <div className="form-row">
-          <label htmlFor="email" className="form-label">
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            className="form-input"
-            value={loginData.email}
-            onChange={(e) =>
-              setLoginData({ ...loginData, email: e.target.value })
-            }
-          />
-        </div>
-        <div className="form-row">
-          <label htmlFor="password" className="form-label">
-          Password
-          </label>
-          <input
-            type="password"
-            id="password"
-            className="form-input"
-            value={loginData.password}
-            onChange={(e) =>
-              setLoginData({ ...loginData, password: e.target.value })
-            }
-          />
-        </div>
-        <button type="submit" className="btn btn-block submit-btn">
-        Login
-        </button>
-      </form>
-    </>
+      <Routes>
+        <Route path="/" element={<h1>Welcome to the Home Page</h1>} />
+        <Route path="/auth" element={<Auth />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
