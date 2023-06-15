b6d22da9f5eb010d07308c0ca884daaaa71c2512
Marina Tilton <m.v.stoyan@gmail.com>
2023-06-15 14:45:12 -0700

    app

--- src/App.js
+++ src/App.js
@@ -3,71 +3,11 @@ import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-ro
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
-
-const rootUrl = 'https://e-commerce-silkland.onrender.com';
+import { handleRegister, handleLogin, handleLogout } from './components/api';
 
 const App = () => {
+  // eslint-disable-next-line
   const [isLoggedIn, setLoggedIn] = useState(false);
-
-  const handleRegister = async (user) => {
-    try {
-      const url = `${rootUrl}/api/v1/auth/register`;
-      const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(user),
-      });
-
-      if (response.ok) {
-        console.log('Registration successful');
-        setLoggedIn(true);
-      } else {
-        console.log('Registration failed');
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const handleLogin = async (user) => {
-    try {
-      const url = `${rootUrl}/api/v1/auth/login`;
-      const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(user),
-      });
-
-      if (response.ok) {
-        console.log('Login successful');
-        setLoggedIn(true);
-      } else {
-        console.log('Login failed');
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const handleLogout = async () => {
-    try {
-      const url = `${rootUrl}/api/v1/auth/logout`;
-      const response = await fetch(url);
-
-      if (response.ok) {
-        console.log('Logout successful');
-        setLoggedIn(false);
-      } else {
-        console.log('Logout failed');
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
 
   return (
     <Router>
@@ -84,13 +24,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route
           path="/login"
-          element={
-            !isLoggedIn ? (
-              <Login handleLogin={handleLogin} />
-            ) : (
-              <Navigate to="/" replace />
-            )
-          }
+          element={!isLoggedIn ? <Login handleLogin={handleLogin} /> : <Navigate to="/" replace />}
         />
         <Route
           path="/register"
